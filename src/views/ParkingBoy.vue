184f05d0e3c8f62fd77494e509bea9838d1ab7c7
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-27 13:12:42 +0800

    [ style ] add tabbar for parkingBoy-mobile

---
+++ src/views/ParkingBoy.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="content">
+        <router-view />
+    </div>
+    <div class="footer">
+      <van-tabbar route>
+        <van-tabbar-item replace to="/order" icon="description">抢单</van-tabbar-item>
+        <van-tabbar-item replace to="/place" icon="logistics">停取</van-tabbar-item>
+        <van-tabbar-item replace to="/history" icon="todo-list-o">历史</van-tabbar-item>
+        <van-tabbar-item replace to="/user" icon="user-o">我的</van-tabbar-item>
+      </van-tabbar> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ParkingBoy",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  created() {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {}
+};
+</script>
+<style lang='' scoped>
+</style>
