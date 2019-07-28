2368c477ff8af67a05255c90bf7700ff3276c2e9
xuexi666666 <235400554@qq.com>
2019-07-28 18:35:32 +0800

    fix change actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,3 @@
-import axios from 'axios';
-import { Toast } from 'vant';
 export default {
-    login({ commit }, users) {
-        axios.post('http://localhost:8080/users?', 'username=' + users.username + '&password=' + users.password)
-            .then(function (response) {
-                Toast.success("login success");
-            })
-            .catch(function (error) {
-                Toast.fail("login failed");
-            })
-    }
+   
 }
