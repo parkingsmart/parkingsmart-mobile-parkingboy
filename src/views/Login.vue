3ea612c5c420652d93783669103210f0b503cda3
waylon ye <waylon.ye@oocl.com>
2019-07-28 21:52:36 +0800

    [style] format page style

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,12 @@
-<template> 
- <div class="loginStyle">
+<template>
+  <div class="loginStyle">
+    <div class="header">
+      <span class="head">Parking Smart</span>
+    </div>
     <div class="loginCenter">
-      <van-cell-group style="background-color: rgb(204, 204, 204);">
-        <div style="display:flex;justify-content:center;">
-        <van-image width="108" height="105" round src="https://img.yzcdn.cn/vant/cat.jpeg" />
-        </div>
+      <van-cell-group>
         <van-field
+          class="input-box"
           style="margin-top:15px;"
           v-model="username"
           clearable
@@ -15,25 +16,19 @@
         />
 
         <van-field
+          class="input-box"
           v-model="password"
           type="password"
           label="密码"
           placeholder="Please input password"
         />
-        <van-button
-          round
-          type="info"
-          size="large"
-          style="margin-top:25px;"
-          @click="submit"
-        >登录</van-button>
+        <van-button round type="info" size="large" class="login-btn" @click="submit">登录</van-button>
       </van-cell-group>
     </div>
   </div>
 </template>
 
 <script>
-import { Toast } from "vant";
 import { login } from "../apis/login";
 export default {
   name: "Login",
@@ -44,23 +39,12 @@ export default {
       password: ""
     };
   },
-
-  components: {},
-
-  computed: {},
-
-  created() {},
-
-  beforeMount() {},
-
-  mounted() {},
-
   methods: {
     submit() {
       if (this.username === "") {
-        Toast.fail("please enter user name");
+        this.$notify("请输入账户");
       } else if (this.password === "") {
-        Toast.fail("please enter password");
+        this.$notify("请输入账户");
       } else {
         const regEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
         const regNumber = /^[0-9]+$/;
@@ -68,15 +52,16 @@ export default {
           login({
             username: this.username,
             password: this.password
-          }).then(res => {
-          this.$store.commit('getEmployeeInfo',res);
-          this.$router.push('/order');
-        })
-        .catch(err => {
-          console.log(err);
-        });
+          })
+            .then(res => {
+              this.$store.commit("getEmployeeInfo", res);
+              this.$router.push("/order");
+            })
+            .catch(() => {
+              this.$notify("账户密码错误");
+            });
         } else {
-          Toast.fail("Input format is wrong, please re-enter");
+          this.$notify("账户密码错误");
         }
       }
     }
@@ -86,7 +71,54 @@ export default {
 };
 </script>
 <style lang='scss' scoped>
-.content{
+.loginStyle {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('../assets/bg.jpg');
+}
+
+.loginCenter {
+  /deep/ .van-cell-group {
+    background-color: transparent;
+  }
+
+  /deep/ .van-field__label {
+    width: 20vw;
+  }
+}
+
+.header {
+  width: 100vw;
+  position: absolute;
   text-align: center;
+  background-color: rgba(0,0,0,0.2);
+}
+
+.content {
+  text-align: center;
+}
+
+[class*="van-hairline"]::after {
+  border: none;
+}
+
+.input-box {
+  width: 80vw;
+  margin: 15px 0;
+  background-color: rgba(0,0,0,0.2);
+
+  /deep/ span, /deep/ input, /deep/ ::-webkit-input-placeholder {
+    color: white;
+  }
+}
+
+.icon {
+  width: 25vw;
+  height: 25vw;
+}
+
+.login-btn {
+  margin-top: 30px;
+  background-color: rgb(69, 149, 230);
 }
 </style>
