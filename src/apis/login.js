108355795078e50881013402e312aaa215ecc988
waylon ye <waylon.ye@oocl.com>
2019-07-30 02:11:25 +0800

    [fix] fix location issue

--- src/apis/login.js
+++ src/apis/login.js
@@ -1,5 +1,5 @@
 import axios from './base.js';
 
 export function login (users){
-  return axios.post('http://localhost:8080/api/employees/login?', 'username=' + users.username + '&password=' + users.password);
+  return axios.post('/api/employees/login?', 'username=' + users.username + '&password=' + users.password);
 }
